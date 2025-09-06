6f6b0f29ef39135b5c0afe5e32b8111ed11125c5
Rosales <140207733+erosales83@users.noreply.github.com>
2025-09-06 13:04:54 -0700

    Updated constructor

--- DataInvestigator.py
+++ DataInvestigator.py
@@ -5,14 +5,11 @@ class DataInvestigator:
 
     def __init__(self, df: pd.DataFrame):
 
-        if not isinstance(df, pd.DataFrame):
-            self.df = None
-            return
-        if df.empty:
-            self.df = None
-            return
+        if isinstance(df, pd.DataFrame) and not df.empty:
+            self.df = df
 
-        self.df = df
+        else:
+            self.df = None
 
     def baseline(self, col):
         pass
