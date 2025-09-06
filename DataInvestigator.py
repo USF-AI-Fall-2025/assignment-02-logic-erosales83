d216b78e102536ff586477ee2de9932b4e3ac8ab
Rosales <140207733+erosales83@users.noreply.github.com>
2025-09-06 13:01:44 -0700

    Progress on constructor, added if statements

--- DataInvestigator.py
+++ DataInvestigator.py
@@ -4,13 +4,21 @@ import pandas as pd
 class DataInvestigator:
 
     def __init__(self, df: pd.DataFrame):
+
+        if not isinstance(df, pd.DataFrame):
+            self.df = None
+            return
+        if df.empty:
+            self.df = None
+            return
+
         self.df = df
 
-    def baseline(col):
+    def baseline(self, col):
         pass
 
-    def corr(col1, col2):
+    def corr(self, col1, col2):
         pass
 
-    def zeroR(col):
+    def zeroR(self, col):
         pass
